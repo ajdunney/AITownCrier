a0fa4d0b4fc8c6b33d08ef0e1a31c9234b9f3591
ajdunney <alex.j.dunne1@gmail.com>
2023-05-26 16:56:00 +0100

    add prompt gen in llm scv

--- src/ai_town_crier/services/language_model_service.py
+++ src/ai_town_crier/services/language_model_service.py
@@ -21,28 +21,43 @@ class AITownCrier:
         return chain.run(query)
 
 
-def run_town_crier(raw_input_path, raw_output_path, clean_output_path):
+def run_town_crier(raw_input_path, raw_output_path, clean_output_path, clean_prompts_output):
     logger.info('Running town crier')
     news_manager = NewsManager(raw_input_path)
     prompt_manager = PromptManager('resources/startup_template.txt')
     model = AITownCrier()
     d = None
     for _, batch in enumerate(news_manager.get_titles(5)):
-        logger.debug(f'Batch: {batch}')
-        response = model.chat_with_prompt(prompt=prompt_manager.create_chat_prompt(), query=batch)
-        logger.debug(f'Response {response}')
-        response_list = [line for line in response.split('\n') if line.strip()]
-        d = add_to_dict(d, batch, response_list)
+       logger.debug(f'Batch: {batch}')
+       response = model.chat_with_prompt(prompt=prompt_manager.create_chat_prompt(), query=batch)
+       logger.debug(f'Response {response}')
+       response_list = [line for line in response.split('\n') if line.strip()]
+       d = add_to_dict(d, batch, response_list)
 
     save_dicts_to_json(d, raw_output_path)
     logger.info('Reopening outputs to check for sensitive content')
     with open(raw_output_path) as f:
-        data = json.load(f)
+       data = json.load(f)
+
     sensitivity_manager = PromptManager('resources/sensitivity_checker_template.txt')
     for key in list(data.keys()):
-        print(key)
-        response = model.chat_with_prompt(prompt=sensitivity_manager.create_chat_prompt(), query=key)
-        print(response)
-        if not check_string(response, 'OK'):
-            del data[key]
+       print(key)
+       response = model.chat_with_prompt(prompt=sensitivity_manager.create_chat_prompt(), query=key)
+       print(response)
+       if not check_string(response, 'OK'):
+           del data[key]
     save_dicts_to_json(data, clean_output_path)
+
+    logger.info('Reopening to create prompts for SD')
+    with open(clean_output_path) as f:
+        data = json.load(f)
+
+    diffusion_prompt_generator = PromptManager('resources/prompt_creator_template.txt')
+    for key in list(data.keys()):
+       print(key)
+       response = model.chat_with_prompt(prompt=diffusion_prompt_generator.create_chat_prompt(), query=key)
+       print(response)
+       data[key] = response
+
+    print(data)
+    save_dicts_to_json(data, clean_prompts_output)
